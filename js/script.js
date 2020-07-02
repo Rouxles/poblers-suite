6f949d798f7a1f9c38b77753f1f7ecf999d9fdd1
Rouxles <19kamh3@gmail.com>
2020-07-02 16:17:05 +0800

    fakerjs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function randomPoblersName() {
-    let name = names[Math.floor(Math.random() * names.length)];
+    let name = faker.name.findName();
     let fname = name.slice(0,name.indexOf(" "));
     let lname = name.slice(name.indexOf(" ")+1, name.indexOf(" ")+2) + "oblers";
     return `${fname} ${lname}`;
